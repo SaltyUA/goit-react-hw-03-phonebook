1d61e7673198f8071e1b0e50cbded5ad1303e708
Vlad <00cmb.ua@gmail.com>
2023-09-26 21:52:46 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,21 @@ import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
   componentDidMount() {
     const contacts = localStorage.getItem('contacts');
-    if (!contacts && JSON.parse(contacts).length) return;
-    this.setState(() => ({
-      contacts: JSON.parse(contacts),
-    }));
+    if (contacts && JSON.parse(contacts).length !== 0)
+      this.setState(() => ({
+        contacts: JSON.parse(contacts),
+      }));
   }
 
   componentDidUpdate(_, prevState) {
